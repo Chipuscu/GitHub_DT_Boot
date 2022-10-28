2978771e521a625b4a789b5a119d3f2e4de0cca8
Chipuscu <nguyendungkt2104@gmail.com>
2022-10-28 08:34:20 +0700

    ND

---
+++ STm32 2710 2/Core/Src/Flash_external.c
@@ -0,0 +1,183 @@
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include	"uart1.h"
+#include	"flash.h"
+#include	"xmodem.h"
+#include	"stdio.h"
+#include	"string.h"
+#include	"uart1.h"
+#include "Flash_External.h"
+
+
+#define _W25QXX_CS_GPIO               GPIOC
+#define _W25QXX_CS_PIN                GPIO_PIN_5
+#define _W25QXX_USE_FREERTOS          1
+#define _W25QXX_DEBUG                 0
+#define FLASH_SPI_DUMMY								0x00
+#define Readdata											0x03
+#define FLASH_SPI_PAGESIZE 						256
+#define PageProgram										 0x02
+
+extern SPI_HandleTypeDef hspi1;
+
+
+uint8_t SPI_Sendata(uint8_t Data)
+{
+	uint8_t ret;
+	uint16_t Timeout=1000;
+	while(__HAL_SPI_GET_FLAG(&hspi1,SPI_FLAG_TXE)==0&&Timeout)
+	{
+			Timeout--;
+		}
+	if(Timeout==0)
+	{
+		return 0;
+	}
+	HAL_SPI_TransmitReceive(&hspi1, &Data, &ret, 1, 100);
+	Timeout=1000;
+	while((__HAL_SPI_GET_FLAG(&hspi1,SPI_FLAG_RXNE)==RESET)&&Timeout)
+	{
+		Timeout--;
+	}
+	return ret;
+}
+//###################################################################################################################
+void Flash_EnableWrite(void)
+{
+	FLASH_Enable();
+	SPI_Sendata(0x06);
+	FLASH_Disable();
+	SPI_Sendata(1);
+}
+//###################################################################################################################
+void Erase_Sector(void)
+{
+	uint16_t Count1=0,Count2=10;
+		while(Count1<Count2)
+			{
+				Flash_Erase_Sector(Count1*4*1024);
+				Count1++;
+				HAL_Delay(50);
+			}
+	
+}
+void Flash_Erase_Sector(uint32_t Address)
+{
+
+	Flash_EnableWrite();
+	//delay1ms();
+	FLASH_Enable();
+	SPI_Sendata(0xD8);
+	/*Gui 3 byte dia chi*/
+	SPI_Sendata((Address >> 16)&0xFF);
+	SPI_Sendata((Address >> 8)&0xFF);
+	SPI_Sendata(Address&0xFF);
+	FLASH_Disable();
+	HAL_Delay(1);
+	Flash_WaitForWriteEnd();
+
+}
+//###################################################################################################################
+void Flash_WaitForWriteEnd(void)
+{
+	uint32_t	TimeOut = 0xFFFFFFF;
+	uint8_t		Status = 0;
+	HAL_Delay(1);
+	FLASH_Enable();
+	SPI_Sendata(0xD8);
+	while(TimeOut)
+		{
+			Status = (uint8_t)SPI_Sendata(FLASH_SPI_DUMMY);		
+			TimeOut--;
+			if((Status & 1) == 0)
+				break;
+		}
+	FLASH_Disable();
+}
+//########################################################################################################
+void Flash_ReadBuffer1(uint32_t Address,char *Buffer,uint16_t Len)
+{
+	FLASH_Enable();
+	
+	SPI_Sendata(Readdata);
+	SPI_Sendata((Address >> 16) & 0xFF);		
+	SPI_Sendata((Address >> 8) & 0xFF);
+	SPI_Sendata(Address  & 0xFF);
+	while(Len--)
+	{
+		*Buffer = SPI_Sendata(FLASH_SPI_DUMMY);	
+		Buffer++;
+	}
+	FLASH_Disable();
+	
+}
+void Flash_ReadBuffer(int Address, uint8_t *Buffer,uint16_t Len)
+{
+	FLASH_Enable();
+	SPI_Sendata(Readdata);
+	SPI_Sendata((Address >> 16)&0xFF);
+	SPI_Sendata((Address >> 8)&0xFF);
+	SPI_Sendata(Address&0xFF);
+	while(Len--)
+	{
+		*Buffer=SPI_Sendata(FLASH_SPI_DUMMY);
+		 Buffer++;
+	}
+	FLASH_Disable();
+}
+//########################################################################################################
+void Flash_Writepage(int Address, uint8_t *Buffer,uint16_t Len)
+{
+	Flash_EnableWrite();
+	FLASH_Enable();
+	SPI_Sendata(PageProgram);
+	SPI_Sendata((Address >> 16)&0xFF);
+	SPI_Sendata((Address >> 8)&0xFF);
+	SPI_Sendata(Address&0xFF);
+	while(Len--)
+	{
+		SPI_Sendata(*Buffer);
+		Buffer++;
+	}
+	FLASH_Disable();
+	//Flash_WaitForWriteEnd();
+}
+//########################################################################################################
+void Flash_WriteBuffer(uint8_t* pBuffer, uint32_t WriteAddr, uint16_t Len)
+{
+  uint16_t Addr = 0, count = 0,CountPage=0;
+	int temp = 0;
+	Addr = WriteAddr % FLASH_SPI_PAGESIZE;	// Da viet den dia chi nay
+	
+	if (Addr+Len<=FLASH_SPI_PAGESIZE) //So luong Byte +dia chi < 1 page
+	{
+			Flash_Writepage((int)WriteAddr,pBuffer,Len);
+			
+	}
+	else
+	{
+		count=FLASH_SPI_PAGESIZE-Addr;// So luong byte chua duoc viet trong page 
+		Flash_Writepage((int)WriteAddr,pBuffer,count); // Viet not vao
+		HAL_Delay(1);
+		temp=(int)(Len-count); // So luong byte con lai
+		CountPage=temp/FLASH_SPI_PAGESIZE; // Dem so trang phai viet
+		WriteAddr+=count;					// Dia chi moi 
+		pBuffer+=count;
+		temp=temp%FLASH_SPI_PAGESIZE; 		//So luong byte thua ra khi viet vao cac page
+		if(CountPage>0)
+		{
+			while(CountPage--)
+			{
+				
+				Flash_Writepage((int)WriteAddr,pBuffer,FLASH_SPI_PAGESIZE);
+				pBuffer+=FLASH_SPI_PAGESIZE;
+				WriteAddr+=FLASH_SPI_PAGESIZE;
+				HAL_Delay(1);
+				
+			}
+		}
+		Flash_Writepage((int)WriteAddr,pBuffer,(uint16_t)temp);
+	}
+}
---
+++ STm32 2710 2/MDK-ARM/STm32/startup_stm32f205xx.d
@@ -0,0 +1,1 @@
+stm32\startup_stm32f205xx.o: startup_stm32f205xx.s
